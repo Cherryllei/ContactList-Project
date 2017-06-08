c78535828d86fc45516271205fe71a0aadc56fff
Cherryllei <chunlei.ku@gmail.com>
2017-06-08 10:48:11 -0700

    after feedback

--- FinalProject/TestContactList.java
+++ FinalProject/TestContactList.java
@@ -12,10 +12,9 @@ import FinalProject.ContactList;
 public class TestContactList {
     public static void main(String[] args) {
         ContactList allContacts = new ContactList();
+        allContacts.loadContactList();
+        
         Contact contact1 = new Contact();
-
-        allContacts.openDisk();
-
         contact1.setLastName("Smith");
         contact1.setFirstName("John");
         contact1.setStreetAddress("235 Fake Rd., Palo Alto");
@@ -23,11 +22,23 @@ public class TestContactList {
         contact1.setPhoneNumber("1234567890");
         contact1.setNotes("Fake notes.");
         System.out.println(contact1);
-
         allContacts.addContact(contact1);
-        allContacts.getAllContacts();
-        allContacts.searchContacts("Smith");
-        allContacts.saveToDisk();
+        
+        Contact contact2 = new Contact();
+        contact2.setLastName("Taylor");
+        contact2.setFirstName("Anna");
+        contact2.setStreetAddress("");
+        contact2.setEmailAddress("");
+        contact2.setPhoneNumber("");
+        contact2.setNotes("");
+        System.out.println(contact2);
+        allContacts.addContact(contact2);
+        
+        System.out.println("Print all contacts: " + allContacts.getAllContacts() + "\n");
+        System.out.println("Search last name Smith:" + allContacts.searchContacts("Smith"));
+        System.out.println("Search last name Taylor:" + allContacts.searchContacts("Taylor") + "\n");
+        System.out.println(allContacts);
+        allContacts.saveContactList();
         System.exit(0);
     }
 }
@@ -39,4 +50,42 @@ Street address: 235 Fake Rd., Palo Alto
 Email: johnsmith@gmail.com
 Phone: 1234567890
 Notes: Fake notes.
+
+Last name: Taylor
+First name: Anna
+Street address: 
+Email: 
+Phone: 
+Notes: 
+
+Print all contacts: [Last name: Smith
+First name: John
+Street address: 235 Fake Rd., Palo Alto
+Email: johnsmith@gmail.com
+Phone: 1234567890
+Notes: Fake notes.
+, Last name: Taylor
+First name: Anna
+Street address: 
+Email: 
+Phone: 
+Notes: 
+]
+
+Search last name Smith:[]
+Search last name Taylor:[]
+
+[Last name: Smith
+First name: John
+Street address: 235 Fake Rd., Palo Alto
+Email: johnsmith@gmail.com
+Phone: 1234567890
+Notes: Fake notes.
+, Last name: Taylor
+First name: Anna
+Street address: 
+Email: 
+Phone: 
+Notes: 
+]
  */
