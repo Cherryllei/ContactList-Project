3280fd1257f7201e78835d4c6979b46f9fc4db2d
erinliao <erin.m.liao@gmail.com>
2017-06-09 15:46:08 -0700

    Commented out TestContactList

--- FinalProject/TestContactList.java
+++ FinalProject/TestContactList.java
@@ -1,77 +1,41 @@
-package FinalProject;
-
-import FinalProject.Contact;
-import FinalProject.ContactList;
-
-/**
- * Creates an object of ContactList and an object of Contact, and calls all
- * methods on them.
- * 
- * @author Chunlei Li
- */
-public class TestContactList {
-	public static void main(String[] args) {
-		ContactList allContacts = new ContactList();
-		allContacts.loadContactList();
-
-		Contact contact1 = new Contact();
-		contact1.setLastName("Smith");
-		contact1.setFirstName("John");
-		contact1.setStreetAddress("235 Fake Rd., Palo Alto");
-		contact1.setEmailAddress("johnsmith@gmail.com");
-		contact1.setPhoneNumber("1234567890");
-		contact1.setNotes("Fake notes.");
-		System.out.println(contact1);
-		allContacts.addContact(contact1);
-
-		Contact contact2 = new Contact();
-		contact2.setLastName("Taylor");
-		contact2.setFirstName("Anna");
-		contact2.setStreetAddress("");
-		contact2.setEmailAddress("");
-		contact2.setPhoneNumber("");
-		contact2.setNotes("");
-		System.out.println(contact2);
-		allContacts.addContact(contact2);
-
-		System.out.println("Search last name Smith:" + allContacts.searchContacts("Smith"));
-		System.out.println("Search last name Taylor:" + allContacts.searchContacts("Taylor") + "\n");
-		System.out.println(allContacts);
-		allContacts.saveContactList();
-		System.exit(0);
-	}
-}
-
-/*
- Last name: Smith
-First name: John
-Street address: 235 Fake Rd., Palo Alto
-Email: johnsmith@gmail.com
-Phone: 1234567890
-Notes: Fake notes.
-
-Last name: Taylor
-First name: Anna
-Street address: 
-Email: 
-Phone: 
-Notes: 
-
-Search last name Smith:[]
-Search last name Taylor:[]
-
-[Last name: Smith
-First name: John
-Street address: 235 Fake Rd., Palo Alto
-Email: johnsmith@gmail.com
-Phone: 1234567890
-Notes: Fake notes.
-, Last name: Taylor
-First name: Anna
-Street address: 
-Email: 
-Phone: 
-Notes: 
-]
-
- */
+//package FinalProject;
+//
+//import java.util.Scanner;
+//
+///**
+// * Creates an object of ContactList and an object of Contact, and calls all
+// * methods on them.
+// * 
+// * @author Chunlei Li
+// */
+//public class TestContactList {
+//	public static void main(String[] args) {
+//		ContactList allContacts = new ContactList();
+//		allContacts.loadContactList();
+//
+//		while (true) {
+//			System.out.println(
+//					"Welcome to The Contact List's main menu. What would you like to do? ");
+//			System.out.println("[1]Add contact" + "\n" + "[2]View all contacts"
+//					+ "\n" + "[3]Search contacts by last name" + "\n"
+//					+ "[4]Quit");
+//			Scanner console = new Scanner(System.in);
+//			int optionNumber = console.nextInt();
+//			if (optionNumber == 1) {
+//				Contact contact1 = new Contact();
+//				contact1.read();
+//				allContacts.addContact(contact1);
+//				System.out.println("Contact saved!" + "\n1");
+//				allContacts.saveContactList();
+//			} else if (optionNumber == 2) {
+//				System.out.println(allContacts);
+//			} else if (optionNumber == 3) {
+//				System.out.println("Please enter the people's last name: ");
+//				allContacts.searchContacts(console.next());
+//			} else {
+//				System.exit(0);
+//			}
+//		}
+//
+//	}
+//}
