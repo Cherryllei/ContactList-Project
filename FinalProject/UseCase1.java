939e6228fc6c8939b0198fd1413e18121f2d1a2e
Juliana <mayiyan@gmail.com>
2017-06-13 11:33:27 -0700

    Format the main. Created a for loop.

--- FinalProject/UseCase1.java
+++ FinalProject/UseCase1.java
@@ -3,47 +3,48 @@ package FinalProject;
 import java.util.Scanner;
 
 /**
- * Creates an object of ContactList and two objects of Contact. After each Contact
- * is created, the ContactList is printed.
+ * Creates an object of ContactList and two objects of Contact. After each
+ * Contact is created, the ContactList is printed.
+ * 
  * @author Chunlei Li
  */
 public class UseCase1 {
-    public static void main(String[] args) {
-        ContactList allContacts = new ContactList();
-        Scanner console = new Scanner(System.in);
+	public static void main(String[] args) {
+		ContactList allContacts = new ContactList();
+		Scanner console = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++){
-            Contact contact = new Contact();
-            String lastName;
-            System.out.print("Please type last name (required): \n" + "> ");
-            lastName = console.nextLine();
-            if (lastName.trim().length() <= 0) {
-                System.out.println("\n" + "Last name is required. \n");
-            } else {
-                contact.setLastName(lastName);
-                
-                System.out.print("Please type a first name: \n" + "> ");
-                contact.setFirstName(console.nextLine());
-        
-                System.out.print("Please type a street address: \n" + "> ");
-                contact.setStreetAddress(console.nextLine());
-        
-                System.out.print("Please type an email: \n" + "> ");
-                contact.setEmailAddress(console.nextLine());
-        
-                System.out.print("Please type a phone number: \n" + "> ");
-                contact.setPhoneNumber(console.nextLine());
-        
-                System.out.print("If you’d like to add some notes, please type them: \n" + "> ");
-                contact.setNotes(console.nextLine());
-        
-                allContacts.addContact(contact);
-    
-                System.out.println("\n" + "Contact saved!" + "\n");
-                System.out.println(allContacts + "\n");
-            }
-        }
-    }
+		for (int i = 0; i < 3; i++) {
+			Contact contact = new Contact();
+			String lastName;
+			System.out.print("Please type last name (required): \n" + "> ");
+			lastName = console.nextLine();
+			if (lastName.trim().length() <= 0) {
+				System.out.println("\n" + "Last name is required. \n");
+			} else {
+				contact.setLastName(lastName);
+
+				System.out.print("Please type a first name: \n" + "> ");
+				contact.setFirstName(console.nextLine());
+
+				System.out.print("Please type a street address: \n" + "> ");
+				contact.setStreetAddress(console.nextLine());
+
+				System.out.print("Please type an email: \n" + "> ");
+				contact.setEmailAddress(console.nextLine());
+
+				System.out.print("Please type a phone number: \n" + "> ");
+				contact.setPhoneNumber(console.nextLine());
+
+				System.out.print("If you’d like to add some notes, please type them: \n" + "> ");
+				contact.setNotes(console.nextLine());
+
+				allContacts.addContact(contact);
+
+				System.out.println("\n" + "Contact saved!" + "\n");
+				System.out.println(allContacts + "\n");
+			}
+		}
+	}
 }
 
 /*
